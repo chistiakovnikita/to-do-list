a64586c51948551225a1bbb7069f7a0608314a69
nikitachistiakov <nikita_chnv@mail.ru>
2022-11-08 20:14:07 +0300

    to do list

---
+++ src/core/Component/Component.js
@@ -0,0 +1,63 @@
+export class Component extends HTMLElement {
+    constructor() {
+        super()
+        this.state = {}
+        this.props = {}
+    }
+
+    setState(callback) {
+        this.state = callback(this.state);
+        this.innerHTML = this.render()
+            .trim()
+            .replaceAll(/true|false/gi, "")
+            .replaceAll(",", "");
+
+
+    }
+
+    connectedCallback() {
+        console.log('asdasd')
+        this.innerHTML = this.render()
+            .trim()
+            .replaceAll(/true|false/gi, "")
+            .replaceAll(",", "");
+        this.componentDidMount()
+        this.registerEvents()
+    }
+
+    disconnectedCallBack() {
+        this.componentWillUnMount()
+    }
+
+    attributeChangeCallback(name, oldValue, newValue) {
+        this.componentWillUpdate(name, oldValue, newValue)
+        this.getAttributeNames().forEach(() => {
+            this.props[name] = this.getAttribute(name)
+        })
+    }
+
+
+    dispatch(type, props) {
+        this.dispatchEvent(new CustomEvent(type, { bubbles: true, detail: props }))
+    }
+
+    registerEvents() {
+       
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUpdate() {
+
+    }
+
+    componentWillUnMount() {
+
+    }
+
+    render() {
+
+    }
+}
---
+++ src/services/database/Database.js
@@ -0,0 +1,50 @@
+import { initializeApp } from "firebase/app";
+import { getFireStore, collection, addDoc, getDocs, doc, updateDoc,deleteDoc } from 'firebase/firestore'
+import { API_KEY } from '../../constants/envValues'
+
+export class Database {
+    constructor() {
+        const firebaseConfig = {
+            apiKey: API_KEY,
+            authDomain: "to-do-list-b0d1c.firebaseapp.com",
+            projectId: "to-do-list-b0d1c",
+            storageBucket: "to-do-list-b0d1c.appspot.com",
+            messagingSenderId: "900825985038",
+            appId: "1:900825985038:web:55da9724b536e60baa0c5f",
+            measurementId: "G-VNSD5XZXT5"
+        };
+
+        const app = initializeApp(firebaseConfig);
+        this._database = getFireStore(app);
+
+    }
+
+    create(collectionKey, body) {
+        const collectionRef = collection(this._database, collectionKey);
+        return addDoc(collectionRef, body)
+    }
+
+    read(collectionKey) {
+        const collectionRef = collection(this._database, collectionKey);
+        return getDocs(collectionRef).then((documents) => {
+            return documents.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        });
+    }
+
+    update(collectionKey, id, body) { 
+        const document = doc(this._database,collectionKey,id);
+        return updateDoc(document, body)
+    }
+
+    delete(collectionKey, id) { 
+        const document = doc(this._database,collectionKey,id);
+        return deleteDoc(document)
+    }
+
+    static getInstance() {
+        if (!Database.instance) {
+            Database.instance = new Database()
+        }
+        return Database.instance
+    }
+}
